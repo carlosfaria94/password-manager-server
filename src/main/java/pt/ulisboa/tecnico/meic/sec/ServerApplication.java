bfcae300ad7908cb549395efe8af761d08296d55
CarlosFaria94 <carlosfaria94@gmail.com>
2017-02-27 11:36:39 +0000

    Init

---
+++ src/main/java/pt/ulisboa/tecnico/meic/sec/ServerApplication.java
@@ -0,0 +1,12 @@
+package pt.ulisboa.tecnico.meic.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}

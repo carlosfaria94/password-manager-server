d6581ccd7a301f33100cb2b5a7b6dc725c35987b
CarlosFaria94 <carlosfaria94@gmail.com>
2017-04-24 17:05:24 +0100

    Store the device id needed to compare in the client

--- src/main/java/pt/ulisboa/tecnico/meic/sec/Password.java
+++ src/main/java/pt/ulisboa/tecnico/meic/sec/Password.java
@@ -22,12 +22,12 @@ public class Password {
     public String username;
     public String password;
 
-    @Transient
-    public String deviceId;
+
 
     @Column(length = 500)
     public String pwdSignature;
 
+    public String deviceId;
     public String versionNumber;
     public String timestamp;
     @Transient
@@ -46,35 +46,28 @@ public class Password {
     Password() { // jpa only
     }
 
-    public Password(User user, String domain, String username, String password, String versionNumber, String pwdSignature, String timestamp, String nonce, String reqSignature) {
+    public Password(User user, String domain, String username, String password, String versionNumber, String deviceId, String pwdSignature, String timestamp, String nonce, String reqSignature) {
         this.domain = domain;
         this.username = username;
         this.password = password;
         this.user = user;
         this.versionNumber = versionNumber;
+        this.deviceId = deviceId;
         this.pwdSignature = pwdSignature;
         this.timestamp = timestamp;
         this.nonce = nonce;
         this.reqSignature = reqSignature;
     }
 
-    public Password(String publicKey, String domain, String username, String password, String pwdSignature, String timestamp, String nonce, String reqSignature) {
-        this.publicKey = publicKey;
-        this.domain = domain;
-        this.username = username;
-        this.password = password;
-        this.pwdSignature = pwdSignature;
-        this.timestamp = timestamp;
-        this.nonce = nonce;
-        this.reqSignature = reqSignature;
-    }
 
-    public Password(String publicKey, String domain, String username, String password, String versionNumber, String pwdSignature, String timestamp, String nonce, String reqSignature) {
+
+    public Password(String publicKey, String domain, String username, String password, String versionNumber, String deviceId, String pwdSignature, String timestamp, String nonce, String reqSignature) {
         this.publicKey = publicKey;
         this.domain = domain;
         this.username = username;
         this.password = password;
         this.versionNumber = versionNumber;
+        this.deviceId = deviceId;
         this.pwdSignature = pwdSignature;
         this.timestamp = timestamp;
         this.nonce = nonce;
@@ -92,6 +85,7 @@ public class Password {
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", versionNumber='" + versionNumber + '\'' +
+                ", deviceId='" + deviceId + '\'' +
                 ", pwdSignature='" + pwdSignature + '\'' +
                 ", nonce='" + nonce + '\'' +
                 ", timestamp='" + timestamp + '\'' +

3a1bd8e737559c390937671f49671df8abd83e55
CarlosFaria94 <carlosfaria94@gmail.com>
2017-05-03 21:54:27 +0100

    Methods to call other replicas

---
+++ src/main/java/pt/ulisboa/tecnico/meic/sec/ServerCallsPool.java
@@ -0,0 +1,123 @@
+package pt.ulisboa.tecnico.meic.sec;
+
+import java.io.IOException;
+
+public class ServerCallsPool {
+    private int initialPort = 3001;
+    private int finalPort = 3004;
+
+    private SingleServerCalls[] singleServerCalls;
+
+    public ServerCallsPool(int initialPort, int finalPort) {
+        this.initialPort = initialPort;
+        this.finalPort = finalPort;
+        init();
+    }
+
+    public ServerCallsPool(int replicas) {
+        this.finalPort = this.initialPort + replicas - 1;
+        init();
+    }
+
+    public ServerCallsPool() {
+        init();
+    }
+
+    public int size() {
+        return singleServerCalls.length;
+    }
+
+    private void init() {
+        singleServerCalls = new SingleServerCalls[finalPort - initialPort + 1];
+        for (int i = 0; i < singleServerCalls.length; i++) {
+            singleServerCalls[i] = new SingleServerCalls(initialPort + i);
+        }
+    }
+
+    public User[] register(User user) throws IOException {
+        Thread[] threads = new Thread[singleServerCalls.length];
+        User[] usersResponses = new User[singleServerCalls.length];
+
+        for (int i = 0; i < singleServerCalls.length || i < threads.length; i++) {
+            int finalI = i;
+            threads[i] = new Thread(() -> {
+                try {
+                    usersResponses[finalI] = singleServerCalls[finalI].register(user);
+                } catch (Exception ignored) {
+                    // If a thread crashed, it's probably connection problems
+                }
+            });
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return usersResponses;
+    }
+
+
+    public Password[] putPassword(Password pwd) throws IOException {
+        Thread[] threads = new Thread[singleServerCalls.length];
+        Password[] passwordsResponse = new Password[singleServerCalls.length];
+        for (int i = 0; i < singleServerCalls.length || i < threads.length; i++) {
+            int finalI = i;
+            threads[i] = new Thread(() -> {
+                try {
+                    passwordsResponse[finalI] = singleServerCalls[finalI].putPassword(pwd);
+                } catch (Exception ignored) {
+                    // If a thread crashed, it's probably connection problems
+                }
+            });
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return passwordsResponse;
+    }
+
+    public Password[] retrievePassword(Password pwd) throws IOException {
+        Thread[] threads = new Thread[singleServerCalls.length];
+        Password[] passwordsResponse = new Password[singleServerCalls.length];
+        for (int i = 0; i < singleServerCalls.length || i < threads.length; i++) {
+            int finalI = i;
+            threads[i] = new Thread(() -> {
+                try {
+                    passwordsResponse[finalI] = singleServerCalls[finalI].retrievePassword(pwd);
+                } catch (Exception ignored) {
+                    // If a thread crashed, it's probably connection problems
+                }
+            });
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return passwordsResponse;
+    }
+
+    //Mockup purpose
+    public void setSingleServerCalls(SingleServerCalls[] singleServerCalls) {
+        this.singleServerCalls = singleServerCalls;
+    }
+}
---
+++ src/main/java/pt/ulisboa/tecnico/meic/sec/SingleServerCalls.java
@@ -0,0 +1,105 @@
+package pt.ulisboa.tecnico.meic.sec;
+
+
+import com.google.gson.Gson;
+import okhttp3.*;
+
+import java.io.IOException;
+
+/**
+ * Methods to interact with the server via a REST service
+ */
+public class SingleServerCalls {
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private OkHttpClient client = new OkHttpClient();
+    private String apiBaseUrl;
+    private Gson json = new Gson();
+
+    SingleServerCalls() {
+        apiBaseUrl = "http://localhost:3001";
+    }
+
+    SingleServerCalls(int port) {
+        this.apiBaseUrl = "http://localhost:" + port;
+    }
+
+    /**
+     * Register user in the server
+     *
+     * @param user - User
+     * @return User - null when user is not successful registered
+     * @throws IOException - when remote server fails to respond
+     */
+    public User register(User user) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json.toJson(user));
+        Request request = new Request.Builder()
+                .url(apiBaseUrl + "/")
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        if (response.isSuccessful()) {
+            //System.out.println("User successful registered: " + newUser.toString());
+            return json.fromJson(response.body().string(), User.class);
+        } else {
+            switch (response.code()) {
+                case 409:
+                    //System.out.println("User already registered.");
+                    break;
+                case 500:
+                    //System.out.println("User not registered. Internal Server error.");
+                    break;
+                default:
+                    //System.out.println("User not registered.");
+                    break;
+            }
+            return null;
+        }
+    }
+
+    /**
+     * Create a new password in server or update
+     *
+     * @param pwd - Password
+     * @return Password
+     * @throws IOException - when remote server fails to respond
+     */
+    public Password putPassword(Password pwd) throws IOException {
+        //System.out.println(pwd);
+        RequestBody body = RequestBody.create(JSON, json.toJson(pwd));
+        Request request = new Request.Builder()
+                .url(apiBaseUrl + "/password")
+                .put(body)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        if (response.isSuccessful()) {
+            //System.out.println("Password successful registered: " + newPassword.toString());
+            return json.fromJson(response.body().string(), Password.class);
+        } else {
+            //System.out.println("Password not registered. HTTP Code: " + response.code());
+            return null;
+        }
+    }
+
+    public Password retrievePassword(Password pwd) throws IOException {
+        String input = json.toJson(pwd);
+
+        RequestBody body = RequestBody.create(JSON, input);
+        Request request = new Request.Builder()
+                .url(apiBaseUrl + "/retrievePassword")
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        if (response.isSuccessful()) {
+            //System.out.println("Password successful retrieved: " + pwdRetrieved.toString());
+            return json.fromJson(response.body().string(), Password.class);
+        } else {
+            //System.out.println("Password not retrieved. HTTP Code: " + response.code());
+            return null;
+        }
+    }
+}
